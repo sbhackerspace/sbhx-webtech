60a826042133245470024dd37dffdd944930f06c
AJ Bahnken <avb.wkyhu@gmail.com>
2012-01-18 11:52:16 -0800

    Added some Code Snippets

---
+++ presentations/introtodjangoslides/code/mysite/hello/views.py
@@ -0,0 +1,21 @@
+# Create your views here.
+
+from django.http import Http404, HttpResponse
+from django.shortcuts import render_to_response
+import datetime
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+def current_datetime(request):
+    current_date = datetime.datetime.now()
+    return render_to_response('current_datetime.html', locals())
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body> In %s hour(s), it will be %s.</body></html>" % (offset,dt)
+    return HttpResponse(html)
---
+++ presentations/introtodjangoslides/code/mysite/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from mysite.hello.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    ('^hello/$', hello),
+    ('^time/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
+    # Example:
+    # (r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
